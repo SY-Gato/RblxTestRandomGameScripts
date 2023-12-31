d009052d5c395b5fc16e514bc34525c523533c30
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-31 14:56:24 -0600

    Update Try1.lua

--- OutputWindowThing/Try1.lua
+++ OutputWindowThing/Try1.lua
@@ -4,12 +4,36 @@ warn("WARNING! THIS PROJECT IS STILL IN BETA, AND MAY HAVE ERRORS.")
 print("------------------------------------------------------")
 print("Testing Script Side..")
 local runServ = game:GetService("RunService")
+local plrs = game:GetService("Players")
+local plr
+local function findPlayerParent(obj)
+  local insa = obj
+  local thingy = nil
+  while true do
+    if insa.Parent == game then
+      warn("Could not find player")
+      break
+    else
+      if insa.Parent:IsA("Player") then
+        thingy = insa.Parent
+        break
+      end
+      insa = insa.Parent
+    end
+  end
+  return thingy
+end
 if runServ:IsClient() then
   print("Script Side Detected! Type: Client!")
   warn("Script Must Be Ran As Server! Fatal Error!")
-  warn("Deleting Self and References...")
-  script:Destroy()
+  error("Fatal Error Occurred.")
 else
   print("Script Side Detected! Type: Server!")
   print("No errors found! Continuing...")
-  
+  plr = findPlayerParent(script)
+  if not plr then
+    error("Could not find player!")
+  elseif plr then
+    print("Player found! "..plr.Name)
+  end
+end
