703ca8994319237e01377b0424545a193e22438f
SY-Gato <148269795+SY-Gato@users.noreply.github.com>
2023-12-31 11:42:55 -0600

    Update Try1.lua

--- OutputWindowThing/Try1.lua
+++ OutputWindowThing/Try1.lua
@@ -1,1 +1,10 @@
-
+print("Output v1 Beta, created by SY-Gato (GitHub, roblox user: master200crate')")
+warn("LICENSED UNDER MIT; Copyright (c) 2023 SY-Gato")
+warn("WARNING! THIS PROJECT IS STILL IN BETA, AND MAY HAVE ERRORS.")
+print("------------------------------------------------------")
+print("Testing Script Side..")
+local runServ = game:GetService("RunService")
+if runServ:IsClient() then
+  print("Script Side Detected! Type: Client!")
+  warn("Script Must Be Ran As Server! Fatal Error!")
+  warn("Deleting Self and References
